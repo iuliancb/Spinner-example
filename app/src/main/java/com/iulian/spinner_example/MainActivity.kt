9ed5239b170c0be86af073f3200d9e17f4a25d5f
Iulian Bujniga <iulian_costin19@yahoo.com>
2020-03-19 20:38:49 -0400

    Initial commit

---
+++ app/src/main/java/com/iulian/spinner_example/MainActivity.kt
@@ -0,0 +1,96 @@
+package com.iulian.spinner_example
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.*
+import kotlinx.android.synthetic.main.activity_main.*
+
+//made quick to solve an issue with sorting arrays in kotlin
+class MainActivity : AppCompatActivity(),AdapterView. OnItemSelectedListener{
+
+    var map = mutableMapOf("question1" to 0, "question2" to 0, "question3" to 0, "question4" to 0)
+    var questions:ArrayList<String> = ArrayList(map.keys)
+
+    var spinner:Spinner? = null
+    var spinner1:Spinner? = null
+
+    var selected:String = "";
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //create spinner 1
+        createSpinner1()
+        //create spinner 2
+        createSpinner2()
+
+    }
+
+    fun createSpinner1(){
+
+        spinner = this.spinner_sample
+        spinner!!.setOnItemSelectedListener(this)
+
+        // Create questions array
+        val spinnerAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, questions)
+        // Set layout to use when the list of choices appear
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        // Set Adapter to Spinner
+        spinner!!.setAdapter(spinnerAdapter)
+    }
+
+    fun createSpinner2(){
+
+        spinner1 = this.spinner_sample1
+        spinner1!!.setOnItemSelectedListener(this)
+
+        // Create questions array
+        val spinnerAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, questions)
+        // Set layout to use when the list of choices appear
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        // Set Adapter to Spinner
+        spinner1!!.setAdapter(spinnerAdapter)
+
+    }
+
+    override fun onItemSelected(arg0: AdapterView<*>, view: View, position: Int, id: Long) {
+
+        //reset the first element that is auto selected by the adaptor - find a better way
+        map.put(questions[position] , 0)
+        if(arg0.id == R.id.spinner_sample)
+        {
+            // create a method to deal with all of this and apply it to all the spinners
+            //reset the listArray before selecting - find a better way
+            for (i in 1..3) {
+                map.put(questions[i] , 0)
+            }
+            // mark the selected question in the map
+            map.put(questions[position], 1)
+            //output values
+            println("map key: " + map.values)
+            //filter map values == 0
+            val filteredKeysMap = map.filterValues { it == 0 }
+            //output map keys
+            println("map key: " + filteredKeysMap.keys)
+            //recreate the arrayList here from filteredKeysMap.keys
+            var test:ArrayList<String> = ArrayList(filteredKeysMap.keys)
+            println("map key: " + test.size)
+            // add filter by map key: in logcat
+            // done in one hour or two so please refactor since this is a POC
+
+        }
+        else if(arg0.id == R.id.spinner_sample1)
+        {
+
+
+
+        }
+
+    }
+
+
+    override fun onNothingSelected(arg0: AdapterView<*>) {
+        println("nothing ")
+    }
+}
